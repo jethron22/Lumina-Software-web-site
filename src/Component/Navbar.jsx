bcd0ea046105a6218b5f7f4562f49166df448f01
jethronbwira@gmail.com <jethronbwira@gmail.com>
2022-10-05 20:10:44 +0200

    feat: debug netlify error hosting

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
-import { MdAccountBalance } from "react-icons/md";
 import gsap from 'gsap';
 
 
@@ -30,23 +29,26 @@ function MyNavbar() {
 
   return (
 
+    
+
     <>
 
-      <Navbar bg="danger" fixed='top' variant="dark" color='white' Navbar-expand-lg>
+      <Navbar bg="danger" fixed='top' variant="dark" color='white'>
 
         <Container >
           <Navbar.Brand href="#logo"> <h4>Lumina <br></br>Software</h4></Navbar.Brand>
           <Nav className="ml-auto" >
-            <Nav.Link as={Link} to={"/accueil"} ><button onMouseEnter={onTouch} onMouseLeave={onGone} type="button" class="btn btn-danger"><strong>Accueil</strong></button></Nav.Link>
-            <Nav.Link as={Link} to={"/logiciels"}><button onMouseEnter={onTouch} onMouseLeave={onGone} type="button" class="btn btn-danger"><strong>Logiciels</strong></button></Nav.Link>
-            <Nav.Link as={Link} to={"/partenaires"}><button onMouseEnter={onTouch} onMouseLeave={onGone} type="button" class="btn btn-danger"><strong>Partenaires</strong></button></Nav.Link>
-            <Nav.Link as={Link} to={"/contacts"}><button onMouseEnter={onTouch} onMouseLeave={onGone} type="button" class="btn btn-danger"><strong>Contacts</strong></button></Nav.Link>
+            <Nav.Link as={Link} to={"/accueil"} ><button onMouseEnter={onTouch} onMouseLeave={onGone} type="button" className="btn btn-danger"><strong>Accueil</strong></button></Nav.Link>
+            <Nav.Link as={Link} to={"/logiciels"}><button onMouseEnter={onTouch} onMouseLeave={onGone} type="button" className="btn btn-danger"><strong>Logiciels</strong></button></Nav.Link>
+            <Nav.Link as={Link} to={"/partenaires"}><button onMouseEnter={onTouch} onMouseLeave={onGone} type="button" className="btn btn-danger"><strong>Partenaires</strong></button></Nav.Link>
+            <Nav.Link as={Link} to={"/contacts"}><button onMouseEnter={onTouch} onMouseLeave={onGone} type="button" className="btn btn-danger"><strong>Contacts</strong></button></Nav.Link>
             <Nav.Link as={Link} to={"/apprentissage"}><Button onMouseEnter={onEnter} onMouseLeave={onLeave} className='docs' variant="warning"><strong className='docs'>Documentation</strong></Button></Nav.Link>
           </Nav>
         </Container>
       </Navbar>
       <br />
     </>
+  
 
   )
 }
